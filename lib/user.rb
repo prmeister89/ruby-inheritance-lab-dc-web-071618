628ee9d354be663b77f68be1b913424aa3037115
Peter Meister <prmeister89@gmail.com>
2018-07-23 14:03:33 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,8 +2,9 @@ class User
   attr_accessor :first_name, :last_name
   
   @@empty_knwledge = []
-  def initialize
+  def initialize(self)
     @first_name = first_name
     @last_name = last_name
+    @empty_knwledge << self
   end
 end
