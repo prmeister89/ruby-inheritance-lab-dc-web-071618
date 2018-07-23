8130bc79c85d58a32e278d5cb3379e07c61cc5eb
Peter Meister <prmeister89@gmail.com>
2018-07-23 14:17:38 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,15 +1,8 @@
 class User
   attr_accessor :first_name, :last_name
   
-  @@empty_knowledge = []
-  
   def initialize
     @first_name = first_name
     @last_name = last_name
-    @@empty_knowledge << self
-  end
-  
-  def self.empty_knowledge
-    @@empty_knowledge
   end
 end
